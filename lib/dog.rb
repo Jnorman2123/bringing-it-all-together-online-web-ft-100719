86af3ef394d832ab5856273c3a130419f528c7d1
Jonathan Norman <jnorman2123@gmail.com>
2019-11-08 16:16:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -69,4 +69,13 @@ class Dog
     end
     dog
   end
+
+  def self.find_by_name(name) 
+    sql = <<-SQL 
+    SELECT * FROM dogs WHERE name = ?
+    SQL
+
+    result = DB[:conn].execute(sql, name)[0] 
+    self.new(id: result[0], name: result[1], breed: result[2])
+  end   
 end
